76889a4cfc4c6e0f54a73b5a73f45e2ae5c10e18
Rifath Khan <rifathkhantheo@gmail.com>
2026-04-01 11:22:23 -0700

    Update sample.py

--- sample.py
+++ sample.py
@@ -1,3 +1,8 @@
+import torch
+
+from model import *
+from config import *
+
 # generate from the model using KV cache
 context = torch.zeros((1, block_size), dtype=torch.long, device=device)
 print(decode(model.generate(context, max_new_tokens, temperature=0.9, top_k=50)[0].tolist()))
