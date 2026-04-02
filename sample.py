ba210779699422c8bc1cd90ad8070252d2b08abe
Rifath Khan <rifathkhantheo@gmail.com>
2026-04-02 03:46:01 -0700

    added an error message to make sure train is run before sample

--- sample.py
+++ sample.py
@@ -1,8 +1,14 @@
+import os
 import torch
 
 from config import *
 from data import *
 from model import *
+
+if not os.path.exists("model.pth"):
+    raise FileNotFoundError(
+        "model.pth not found. Run `python train.py` first to train the model and create model.pth. Then run `python sample.py`."
+    )
 
 model = MyGPT()
 model.load_state_dict(torch.load("model.pth", map_location=device))
